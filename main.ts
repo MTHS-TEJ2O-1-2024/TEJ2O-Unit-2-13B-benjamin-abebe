1aa4c8d3960596b2bfe993765a2fb1551dc9a99e
benjamin-abebe <benjamin.abebe@mths.ca>
2024-10-30 11:24:21 -0400

    Made change on typescript file.

--- main.ts
+++ main.ts
@@ -13,60 +13,62 @@ let numberOfCountDown: number
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
-neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.clear()  // Clear all pixels to off
 
-// when button a is pressed
-input.onButtonPressed(Button.A, function() {
-
+// when button A is pressed
+input.onButtonPressed(Button.A, function () {
     // the amount of Count down numbers
     numberOfCountDown = 4
 
-    while (numberOfCountDown > 0) {
-        numberOfCountDown = numberOfCountDown - 1
-
-        if (numberOfCountDown = 4) {
+    while (numberOfCountDown >= 0) {  // Change to >= for countdown including 0
+        if (numberOfCountDown == 4) {
             neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
             neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
             neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
             neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
             basic.showString("4")
-            neopixelStrip.show()
         }
 
-        if (numberOfCountDown = 3) {
+        if (numberOfCountDown === 3) {
             neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
             neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
             neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
             neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
             basic.showString("3")
-            neopixelStrip.show()
         }
 
-        if (numberOfCountDown = 2) {
+        if (numberOfCountDown == 2) {
             basic.showString("2")
-            pause(500)
             neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
             neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
             neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
             neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-            neopixelStrip.show()
         }
 
-        if (numberOfCountDown = 1) {
+        if (numberOfCountDown == 1) {
             basic.showString("1")
-            pause(500)
             neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
             neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
             neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
             neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
-            neopixelStrip.show()
         }
+
+        if (numberOfCountDown == 0) {
+            basic.showString("0")
+            neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+            neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+        }
+
+        neopixelStrip.show()  // Update neopixels
+        basic.pause(500)  // Pause for half a second
 
         // clear screen and show happy face
         basic.clearScreen()
         basic.showIcon(IconNames.Happy)
+
+        // Decrement the countdown
+        numberOfCountDown--;  // Decrement the countdown
     }
-    })
+});
